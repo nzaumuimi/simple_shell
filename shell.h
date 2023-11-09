f446a5bcbff6aec16228eb85b8ab960fa395080d
ghost38143 <ghost38143@gmail.com>
2023-11-09 14:13:17 -0800

    changes

--- shell.h
+++ shell.h
@@ -5,9 +5,10 @@
 #include <stdlib.h>/*const in prog termonation,malloc*/
 #include <string.h>/*this will help in length*/
 #include <stdio.h>
-#include <sys/wait.h>
+#include <sys/wait.h>/*this enables the wait process for the the parent*/
 void prompter_display(void);
 void printer(const char *string);
 void read_command(char *command, size_t size);
 void executer_command(const char *command);
+void executer_arguments(const char *arguments);
 #endif
